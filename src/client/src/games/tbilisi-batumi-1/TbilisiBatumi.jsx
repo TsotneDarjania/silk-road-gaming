84a87923bb3178d9947878bb035b6b6fb92d0b5c
= <=>
2023-06-19 16:22:16 +0400

    .

--- src/client/src/games/tbilisi-batumi-1/TbilisiBatumi.jsx
+++ src/client/src/games/tbilisi-batumi-1/TbilisiBatumi.jsx
@@ -52,8 +52,8 @@ export const TbilisiBatumi = () => {
   const getCanvasSize = () => {
     if (isIOS()) {
       const canvasWidth = isLandscapeOrientation()
-        ? window.outerWidth - hideWidth
-        : window.outerWidth - hideWidth;
+        ? window.outerHeight - hideHeight
+        : window.outerHeight - hideHeight;
       const canvasHeight = isLandscapeOrientation()
         ? window.outerHeight - hideHeight
         : window.outerHeight - hideHeight;
