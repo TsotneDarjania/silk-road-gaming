8d5f9b4006bc8692313708bcf7b505594a61e45c
Nutsa Julakidze <julakidzenu@gmail.com>
2023-06-20 22:44:10 +0400

    add art games component

--- src/client/src/pages/homeMenu/components/artGame/ArtGames.jsx
+++ src/client/src/pages/homeMenu/components/artGame/ArtGames.jsx
@@ -1,24 +1,62 @@
-import React, {useState} from 'react'
-import style from './artGames.module.css'
-import ArtGameContainer from './components/ArtGameContainer'
-import Shadow from '../../../../components/Shadow'
+import React, { useState, useEffect } from "react";
+import style from "./artGames.module.css";
+import ArtGameContainer from "./components/ArtGameContainer";
+import Shadow from "../../../../components/Shadow";
 
 const ArtGames = () => {
-  const [show, setShow] = useState(false)  
+  const [show, setShow] = useState(false);
+  const [active, setActive] = useState([false, false]);
+  const [showShadow, setShowShadow] = useState(false);
+
+  const handleFullScreen = (videoId) => {
+    setShowShadow(!showShadow);
+    setActive((active) => {
+      return active.map((value, i) => {
+        return i === videoId ? !value : value;
+      });
+    });
+  };
+
+  useEffect(() => {
+    showShadow === false &&
+      setActive((active) => {
+        return active.map((value) => {
+          return (value = false);
+        });
+      });
+  }, [showShadow]);
+  
   const shadowProperty = {
     opacity: 0.5,
     transition: "0.5s",
-    show: show,
-    setShow: setShow,
+    show: showShadow || show,
+    setShow: setShow || setShowShadow,
+  };
+
+  const artGameContainers = () => {
+    const quantity = 5;
+    
+    const elements = Array.from({ length: quantity }, (_, i) => (
+      <ArtGameContainer
+        key={i}
+        show={show}
+        setShow={setShow}
+        active={active}
+        handleFullScreen={handleFullScreen}
+        videoId={i}
+        setShowShadow={setShowShadow}
+      />
+    ));
+  
+    return elements;
   };
 
   return (
     <div className={style.artGames}>
-        <Shadow props={shadowProperty}/>
-        <ArtGameContainer show={show} setShow={setShow}/>
-        <ArtGameContainer show={show} setShow={setShow}/>
+      <Shadow props={shadowProperty} />
+      {artGameContainers()}
     </div>
-  )
-}
+  );
+};
 
-export default ArtGames
+export default ArtGames;
--- src/client/src/pages/homeMenu/components/artGame/components/ArtGameContainer.jsx
+++ src/client/src/pages/homeMenu/components/artGame/components/ArtGameContainer.jsx
@@ -1,16 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import style from "./artGameContainer.module.css";
 import gamesInfo from "../../../../../data/gamesInfo.json";
 import gameVideo from "../../../videos/1.mp4";
 import { AiFillDislike, AiFillLike } from "react-icons/ai";
 import { FaCommentAlt } from "react-icons/fa";
-import {MdLeaderboard} from 'react-icons/md'
+import { MdLeaderboard } from "react-icons/md";
+import { BsFullscreen } from "react-icons/bs";
 import ModalForComments from "../../../../../components/ModalForComments";
+import SliderComponent from "../../../../../components/SliderComponent";
+import image from "../../../images/background.jpg";
 
 const ArtGameContainer = (props) => {
-  const description =
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris at dictum ex, vitae ullamcorper magna. Mauris sed ante blandit, commodo sapien non, bibendum neque. Integer pellentesque sit amet purus non lobortis. Phasellus dui risus, ultrices eget lectus vitae, tristique viverra elit. Nunc gravida varius diam at lacinia. Nulla luctus tristique elit, eu bibendum nibh sollicitudin sed. Praesent sed rhoncus purus.";
-  console.log(description.length);
+  const description = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris at dictum ex,
+     vitae ullamcorper magna. Mauris sed ante blandit, commodo sapien non, bibendum neque. 
+     Integer pellentesque sit amet purus non lobortis. Phasellus dui risus, ultrices eget lectus vitae, 
+     tristique viverra elit. Nunc gravida varius diam at lacinia. Nulla luctus tristique elit, eu bibendum nibh sollicitudin sed. 
+     Praesent sed rhoncus purus.`;
+
+  const images = [image, image, image, image, image, image];
 
   return (
     <div className={style.artGameContainer}>
@@ -31,7 +38,9 @@ const ArtGameContainer = (props) => {
             {description}
           </h3>
         </div>
-        <div className={style.artGameGallery}></div>          
+        <div className={style.artGameGallery}>
+          <SliderComponent images={images} />
+        </div>
         <button
           onClick={() => {
             window.open("http://localhost:3000" + gamesInfo.lastGame.url);
@@ -41,32 +50,40 @@ const ArtGameContainer = (props) => {
           Play
         </button>
         <div className={style.indicators}>
-        <ul>
-          <li className={style.likeIcon}>
-            <AiFillLike /> <span className={style.number}> 0 </span>
-          </li>
+          <ul>
+            <li className={style.likeIcon}>
+              <AiFillLike /> <span className={style.number}> 0 </span>
+            </li>
 
-          <li>
-            <div className={style.dislikeIcon}><AiFillDislike /></div>
-            <span className={style.number}> 0 </span>
-          </li>
+            <li>
+              <div className={style.dislikeIcon}>
+                <AiFillDislike />
+              </div>
+              <span className={style.number}> 0 </span>
+            </li>
 
-          <li className={style.commentIcon}>
-            <FaCommentAlt onClick={() => props.setShow(true)}/>
-          </li>
+            <li className={style.commentIcon}>
+              <FaCommentAlt onClick={() => props.setShow(true)} />
+            </li>
 
-          <li>
-            <MdLeaderboard/>
-          </li>
-        </ul>
+            <li>
+              <MdLeaderboard />
+            </li>
+          </ul>
+        </div>
       </div>
-      </div>
-      <div className={style.rightContainer}>
-        <video className={style.gameVideo} loop autoPlay>
-          <source src={gameVideo} type="video/mp4" />
-        </video>
-      </div>
-      <ModalForComments show={props.show}/>
+
+        <div className={`${style.rightContainer} ${props.active[props.videoId] === true ? style.activeVideo : ''}`}>
+          <div className={style.videoContainer}>
+            <video className={style.gameVideo} loop autoPlay>
+              <source src={gameVideo} type="video/mp4" />
+            </video>
+            <div className={style.fullScreenIcon}>
+              <BsFullscreen onClick={() => props.handleFullScreen(props.videoId)} />
+            </div>
+          </div>
+        </div>
+      <ModalForComments show={props.show} />
     </div>
   );
 };
