e618f85c810332794b88eeb0dd86535b60ce1005
Nutsa Julakidze <julakidzenu@gmail.com>
2023-06-30 20:59:44 +0400

    make colors and backgroundcolor in root

--- src/client/src/pages/homeMenu/PageComponents/miniGames/components/gameContainer.jsx
+++ src/client/src/pages/homeMenu/PageComponents/miniGames/components/gameContainer.jsx
@@ -1,8 +1,7 @@
-import { useContext, useState } from "react";
+import { useState } from "react";
 import style from "./gameContainer.module.css";
 import ModalForComments from "../../../../../components/commentsModal/ModalForComments";
 import AuthenticationModal from "../../../../../components/autenticationModal/AuthenticationModal";
-import UserContext from "../../../../../context/UserContext";
 import bgImage from "../../../images/games/miniGames/wallpapers/1.jpg";
 import Indicators from "../../../components/Indicators";
 import LinkButton from "../../../../../components/buttons/LinkButton";
@@ -10,8 +9,6 @@ import LinkButton from "../../../../../components/buttons/LinkButton";
 const GameContainer = (props) => {
   const [showAutenticationModal, setShowAutenticationModal] = useState(false);
   const [showCommentsModal, setShowCommentsModal] = useState(false);
-
-  const userContext = useContext(UserContext);
 
   return (
     <div className={style.gameContainer}>
