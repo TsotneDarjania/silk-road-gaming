92ad48d2638d8c765826504cf490acbb9d5c7c3f
= <=>
2023-07-05 13:20:35 +0400

    init love  chaser project

---
+++ src/client/src/games/love-chaser/LoveChaser.jsx
@@ -0,0 +1,35 @@
+import React, { useEffect, useRef, useState } from "react";
+import Phaser from "phaser";
+import style from "./style.module.css";
+
+import { Preload } from "./scenes/preload";
+
+export const LoveChaser = () => {
+  const canvasContainer = useRef(null);
+
+  useEffect(() => {
+    if (!canvasContainer.current) return;
+
+    const game = new Phaser.Game({
+      dom: { createContainer: true },
+      physics: {
+        default: "arcade",
+      },
+      parent: canvasContainer.current,
+      type: Phaser.AUTO,
+      scale: {
+        mode: Phaser.Scale.NONE,
+        autoCenter: Phaser.Scale.CENTER_BOTH,
+        width: 700,
+        height: 700,
+      },
+
+      backgroundColor: 0x19053b,
+      scene: [Preload],
+    });
+
+    return () => game.destroy(true, false);
+  }, []);
+
+  return <div ref={canvasContainer} className={style.canvas}></div>;
+};
