fd4f44bcabc481f02d38298e7a9884b19c97b840
Nutsa Julakidze <julakidzenu@gmail.com>
2023-07-18 18:31:07 +0400

    add donation page

--- src/client/src/index.tsx
+++ src/client/src/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import ReactDOM from "react-dom/client";
 import "./global.css";
 import App from "./App";
@@ -6,6 +6,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { TbilisiBatumi } from "./games/tbilisi-batumi-1/TbilisiBatumi";
 import { Bagrationi } from "./games/bagrationi/Bagrationi";
 import { LoveChaser } from "./games/love-chaser/LoveChaser";
+import DonationPage from "./pages/donationPage/DonationPage";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
@@ -14,6 +15,7 @@ root.render(
   <Router>
     <Routes>
       <Route path="/" element={<App />} />
+      <Route path="/donation" element={<DonationPage />} />
       <Route path="/games/love-chaser" element={<LoveChaser />} />
       <Route path="/games/bagrationi" element={<Bagrationi />} />
       <Route path="/games/batumisken" element={<TbilisiBatumi />} />
