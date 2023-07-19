02bf0c7b5366b755c6a1de293e666d60473e421f
Nutsa Julakidze <julakidzenu@gmail.com>
2023-07-19 18:35:45 +0400

    add images of animation

--- src/client/src/components/effects/background/Background.jsx
+++ src/client/src/components/effects/background/Background.jsx
@@ -1,28 +1,36 @@
-import React from 'react'
-import style from './background.module.css'
-import Item from './components/Item'
-import { getRandomFloat } from '../../../games/tbilisi-batumi-1/helper/tatukaMath'
+import React, { useEffect, useState } from "react";
+import style from "./background.module.css";
+import Item from "./components/Item";
+import { getRandomFloat } from "../../../games/tbilisi-batumi-1/helper/tatukaMath";
 
-const Background = ({images}) => {
-    
-    const getItems = () => {
-        const length = window.innerWidth / 100 * 2
-        const items = []
+const Background = React.memo(({ images }) => {
+  const [getItems, setGetItems] = useState(null);
 
-        for (let i = 0; i < length; i++) {
-            const animationDelay = getRandomFloat(i, i + 2)
-            const item = <Item image={images[0]} key={`item${i}`} left={getRandomFloat(0, window.innerWidth)} animationDelay={animationDelay}/>
-            items.push(item)
-        }
-        return items
-}
-    
+  useEffect(() => {
+    setGetItems(() => {
+      const length = (window.innerWidth / 100) * 2;
+      const items = [];
+      let imagesIndex = 0
 
-  return (
-    <div className={style.backgroundContainer}>
-      {getItems()}
-    </div>
-  )
-}
+      for (let i = 0; i < length; i++) {
+        const animationDelay = getRandomFloat(i, i + 2);
+        const item = (
+          <Item
+            image={images[imagesIndex]}
+            key={`item${i}`}
+            left={getRandomFloat(0, window.innerWidth)}
+            animationDelay={animationDelay}
+          />
+        );
+        items.push(item);
+        imagesIndex = imagesIndex >= images.length - 1 ? 0 : imagesIndex + 1
+      }
+      console.log("get items");
+      return items;
+    });
+  }, []);
 
-export default Background
+  return <div className={style.backgroundContainer}>{getItems}</div>;
+});
+
+export default Background;
